6e3b609ce8bd113a67acfcb3f0fd14cbdd699501
EstelleUbi <estelle.tourillon@ubidreams.com>
2021-07-23 18:54:26 +0200

    refactor: ajout react-html-parser
    - Suppression du dangerouslySetInnerHTML
    - Utilisation de la lib react-html-parser en substitut
    - Création d'un helper pour vérifier que la traduction existe

--- src/components/text-container.js
+++ src/components/text-container.js
@@ -1,34 +1,44 @@
+import useTranslation from 'next-translate/useTranslation'
+import ReactHtmlParser from 'react-html-parser'
+
+//check for more informations: https://github.com/vinissimus/next-translate/issues/429
+const checkIfTraductionExists = (t, config, section, field) => {
+  return config && config[field] ? config[field] : t(section + `.${field}`, {}, { fallback: '' })
+}
+
 /**
  * Zone de texte générale, option : titre, sous titre, description
  * @param config (données sous la forme d'un objet (title, subtitle, description))
  * @param alignText: propriété basée sur l'alignement du texte
  * @param customStyle appliqué sur la div globale
  */
-const TextContainer = ({ config, alignText, className = '' }) => {
-  const { title, subtitle, content, footer } = config
+const TextContainer = ({ namespace = null, config = null, alignText, className = '' }) => {
+  const { t } = useTranslation(namespace && namespace.name)
+  const section = namespace && namespace.section
 
-  function createMarkup() {
-    return { __html: content }
+  const data = {
+    title: checkIfTraductionExists(t, config, section, 'title'),
+    subtitle: checkIfTraductionExists(t, config, section, 'subtitle'),
+    content: checkIfTraductionExists(t, config, section, 'content'),
+    footer: checkIfTraductionExists(t, config, section, 'footer')
   }
 
   return (
     <div className={`${className} text-${alignText}`}>
-      {title && <h2 className='fw-bold'>{title}</h2>}
+      {data.title && <h2 className='fw-bold'>{data.title}</h2>}
 
-      {subtitle && <p className='text-muted mb-6 mb-md-8'>{subtitle}</p>}
+      {data.subtitle && <p className='text-muted mb-6 mb-md-8'>{data.subtitle}</p>}
 
-      {content && (
-        <div
-          className='text-gray-700'
-          style={{ whiteSpace: 'pre-line' }}
-          dangerouslySetInnerHTML={createMarkup()}
-        ></div>
+      {data.content && (
+        <div className='text-gray-700' style={{ whiteSpace: 'pre-line' }}>
+          {ReactHtmlParser(data.content)}
+        </div>
       )}
 
-      {footer && (
+      {data.footer && (
         <footer className='blockquote-footer mt-3'>
           <span className='h6 text-uppercase' style={{ whiteSpace: 'pre-line' }}>
-            {footer}
+            {data.footer}
           </span>
         </footer>
       )}
