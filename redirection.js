8d69c7156155413ce9c74232bdd5346165d5f3a9
EstelleUbi <estelle.tourillon@ubidreams.com>
2021-10-28 15:50:34 +0200

    fix : redirection bug

--- redirection.js
+++ redirection.js
@@ -1,307 +1,307 @@
 module.exports = [
   /**************************** POST **************************************/
   {
-    source: '/en/blog-2/',
+    source: '/blog-2',
     destination: '/en/blog',
     permanent: true
   },
   {
-    source: '/en/2018/06/19/5-expressions-a-connaitre-pour-comprendre-un-developpeur-2/',
+    source: '/2018/06/19/5-expressions-a-connaitre-pour-comprendre-un-developpeur-2',
     destination: '/en/blog/5-expressions-to-know-to-understand-a-developer',
     permanent: true
   },
   {
-    source: '/2018/06/06/5-expressions-a-connaitre-pour-comprendre-un-developpeur/',
+    source: '/2018/06/06/5-expressions-a-connaitre-pour-comprendre-un-developpeur',
     destination: '/blog/5-expressions-a-connaitre-pour-comprendre-un-developpeur',
     permanent: true
   },
   {
-    source: '/2019/02/14/ubidreams-salon-techinnov-2018/',
+    source: '/2019/02/14/ubidreams-salon-techinnov-2018',
     destination: '/blog/ubidreams-au-salon-techinnov-2018',
     permanent: true
   },
   {
-    source: '/en/2019/07/02/connected-mortorway-project-road-safety/',
+    source: '/2019/07/02/connected-mortorway-project-road-safety',
     destination: '/en/blog/connected-mortorway-project-road-safety',
     permanent: true
   },
   {
-    source: '/2019/07/02/projet-autoroute-connectee-securite-routiere/',
+    source: '/2019/07/02/projet-autoroute-connectee-securite-routiere',
     destination: '/blog/projet-autoroute-connectee-securite-routiere',
     permanent: true
   },
   {
-    source: '/2020/03/17/information-covid-19/',
+    source: '/2020/03/17/information-covid-19',
     destination: '/blog/information-covid-19',
     permanent: true
   },
   {
-    source: '/2020/08/13/ubisea/',
+    source: '/2020/08/13/ubisea',
     destination: '/blog/ubisea-bouee-connectee',
     permanent: true
   },
   {
-    source: '/2020/07/07/business-meeting-saint-nae/',
+    source: '/2020/07/07/business-meeting-saint-nae',
     destination: '/blog/business-meeting-saint-nazaire',
     permanent: true
   },
   {
-    source: '/2021/06/25/typescript-langage-type-open-source/',
+    source: '/2021/06/25/typescript-langage-type-open-source',
     destination: '/blog/typescript-le-langage-de-programmation-open-source-javascript',
     permanent: true
   },
   {
-    source: '/2021/07/05/le-seo-pourquoi-optimiser-son-site-web/',
+    source: '/2021/07/05/le-seo-pourquoi-optimiser-son-site-web',
     destination: '/blog/seo-pourquoi-optimiser-son-site-web',
     permanent: true
   },
 
   /**************************** PAGE LEGAL **************************************/
   {
-    source: '/en/cookies-politic/',
+    source: '/cookies-politic',
     destination: '/',
     permanent: true
   },
   {
-    source: '/gdpr-policy/',
+    source: '/gdpr-policy',
     destination: '/en/privacy',
     permanent: true
   },
   {
-    source: '/mentions-legales/',
+    source: '/mentions-legales',
     destination: '/mentions-legales',
     permanent: true
   },
   {
-    source: '/en/legal-mentions/',
+    source: '/legal-mentions',
     destination: '/en/legal',
     permanent: true
   },
   {
-    source: '/politique-de-cookie/',
+    source: '/politique-de-cookie',
     destination: '/politique-de-cookie',
     permanent: true
   },
   /********************** REFERENCES *******************************************/
   {
-    source: '/reference/',
+    source: '/reference',
     destination: '/references',
     permanent: true
   },
   {
-    source: '/april-marine/',
+    source: '/april-marine',
     destination: '/references/april-marine-assistance',
     permanent: true
   },
   {
-    source: '/en/april-marine-en/',
+    source: '/april-marine-en',
     destination: '/en/references/april-marine-assistance',
     permanent: true
   },
   {
-    source: '/marine-assistance/',
+    source: '/marine-assistance',
     destination: '/references/marine-assistance',
     permanent: true
   },
   {
-    source: '/en/marine-assistance-en/',
+    source: '/marine-assistance-en',
     destination: '/en/references/marine-assistance',
     permanent: true
   },
   {
-    source: '/en/proviacal-en/',
+    source: '/proviacal-en',
     destination: '/en/references/proviacal',
     permanent: true
   },
   {
-    source: '/proviacal/',
+    source: '/proviacal',
     destination: '/references/proviacal',
     permanent: true
   },
   {
-    source: '/en/exposure-rwanda-en/',
+    source: '/exposure-rwanda-en',
     destination: '/en/references/rwanda-20-years-after',
     permanent: true
   },
   {
-    source: '/exposition-rwanda/',
+    source: '/exposition-rwanda',
     destination: '/references/rwanda-20-ans-apres',
     permanent: true
   },
   {
-    source: '/en/sdlp-en/',
+    source: '/sdlp-en',
     destination: '/en/references/sdlp',
     permanent: true
   },
   {
-    source: '/sdlp/',
+    source: '/sdlp',
     destination: '/references/sdlp',
     permanent: true
   },
   {
-    source: '/sugg/',
+    source: '/sugg',
     destination: '/references/sugg1144-et-suggpro',
     permanent: true
   },
   {
-    source: '/en/sugg-2/',
+    source: '/sugg-2',
     destination: '/en/references/sugg1144-et-suggpro',
     permanent: true
   },
   {
-    source: '/en/e-longlife-en/',
+    source: '/e-longlife-en',
     destination: '/en/references/e-longlife',
     permanent: true
   },
   {
-    source: '/e-longlife/',
+    source: '/e-longlife',
     destination: '/references/e-longlife',
     permanent: true
   },
   {
-    source: '/en/pre-ventes-port-de-peche-en/',
+    source: '/pre-ventes-port-de-peche-en',
     destination: '/en/references/preventes',
     permanent: true
   },
   {
-    source: '/pre-ventes-port-de-peche/',
+    source: '/pre-ventes-port-de-peche',
     destination: '/references/preventes',
     permanent: true
   },
   {
-    source: '/en/sea-tags-en/',
+    source: '/sea-tags-en',
     destination: '/en/references/sea-tags',
     permanent: true
   },
   {
-    source: '/sea-tags/',
+    source: '/sea-tags',
     destination: '/references/sea-tags',
     permanent: true
   },
   {
-    source: '/en/mr-voyage-en/',
+    source: '/mr-voyage-en',
     destination: '/en/references/mr-voyage',
     permanent: true
   },
   {
-    source: '/mr-voyage/',
+    source: '/mr-voyage',
     destination: '/references/mr-voyage',
     permanent: true
   },
   {
-    source: '/en/formerstudents/',
+    source: '/formerstudents',
     destination: '/en/references/ubinetwork',
     permanent: true
   },
   {
-    source: '/ubinetwork/',
+    source: '/ubinetwork',
     destination: '/references/ubinetwork',
     permanent: true
   },
   {
-    source: '/amoco/',
+    source: '/amoco',
     destination: '/references/amoco',
     permanent: true
   },
   {
-    source: '/en/amoco-en/',
+    source: '/amoco-en',
     destination: '/en/references/amoco',
     permanent: true
   },
   /**************************** PAGES **************************************/
   //HOME
   {
-    source: '/en/homepage/',
+    source: '/homepage',
     destination: '/en',
     permanent: true
   },
   //outils
   {
-    source: '/en/tools/',
+    source: '/tools',
     destination: '/en/expertises/development/the-different-development-modes',
     permanent: true
   },
   {
-    source: '/le-mode-natif-multiplateforme-react-native/',
+    source: '/le-mode-natif-multiplateforme-react-native',
     destination: '/expertises/development/les-differents-modes-de-developpements',
     permanent: true
   },
   //contact
   {
-    source: '/en/ask-us-something-or-a-cost-estimate/',
+    source: '/ask-us-something-or-a-cost-estimate',
     destination: '/en/contact',
     permanent: true
   },
   //Agence
   {
-    source: '/en/ubidreams-2/',
+    source: '/ubidreams-2',
     destination: '/en/agency',
     permanent: true
   },
   {
-    source: '/ubidreams/',
+    source: '/ubidreams',
     destination: '/agency',
     permanent: true
   },
   //Expertises
   {
-    source: '/en/services-2/',
+    source: '/services-2',
     destination: '/en/expertises',
     permanent: true
   },
   {
-    source: '/services/',
+    source: '/services',
     destination: '/expertises',
     permanent: true
   },
   //technologies
   {
-    source: '/beacons/',
+    source: '/beacons',
     destination: '/expertises/internet-of-things/beacon-votre-projet-iot-sur-mesure',
     permanent: true
   },
   {
-    source: '/en/beacons-2/',
+    source: '/beacons-2',
     destination: '/en/expertises/internet-of-things/beacon-your-customized-iot-project',
     permanent: true
   },
   {
-    source: '/lora/',
+    source: '/lora',
     destination: '/expertises/internet-of-things/votre-projet-d-objet-connecte-avec-lora',
     permanent: true
   },
   {
-    source: '/react-js/',
+    source: '/react-js',
     destination: '/expertises/development/react-js',
     permanent: true
   },
   {
-    source: '/django-python/',
+    source: '/django-python',
     destination: '/expertises/development/notre-expertise-django-python-a-votre-service',
     permanent: true
   },
   {
-    source: '/react-native/',
+    source: '/react-native',
     destination: '/expertises/development/react-native-fr',
     permanent: true
   },
   {
-    source: '/cloud-edge-ou-fog-computing/',
+    source: '/cloud-edge-ou-fog-computing',
     destination: '/expertises/internet-of-things/ensemble-construisons-votre-architecture-iot',
     permanent: true
   },
   {
-    source: '/pwa/',
+    source: '/pwa',
     destination: '/expertises/development/qu-est-ce-qu-une-pwa-progressive-web-app',
     permanent: true
   },
   {
-    source: '/ant-design/',
+    source: '/ant-design',
     destination: '/expertises/development/votre-design-d-interface-personnalise-grace-a-ant-design',
     permanent: true
   },
   {
-    source: '/produits-solutions-digitales/',
+    source: '/produits-solutions-digitales',
     destination: '/en/expertises/development',
     permanent: true
   },
@@ -309,23 +309,23 @@ module.exports = [
   /**************************** AUTRE **************************************/
   {
     source:
-      '/creation-de-logiciels-et-progiciels-metiers-dapplications-mobiles-etudes-de-faisabilite-et-proof-of-concept/',
+      '/creation-de-logiciels-et-progiciels-metiers-dapplications-mobiles-etudes-de-faisabilite-et-proof-of-concept',
     destination: '/expertises/development',
     permanent: true
   },
   {
-    source: '/en/feasibility-study-proof-of-concept-mobile-apps-development-software-and-software-packages%e2%80%8b/',
+    source: '/feasibility-study-proof-of-concept-mobile-apps-development-software-and-software-packages%e2%80%8b',
     destination: '/en/expertises/development',
     permanent: true
   },
 
   {
-    source: '/progiciels-et-logiciels-metiers/',
+    source: '/progiciels-et-logiciels-metiers',
     destination: '/expertises/development',
     permanent: true
   },
   {
-    source: '/en/what-is-a-software-package/',
+    source: '/what-is-a-software-package',
     destination: '/en/expertises/development',
     permanent: true
   }
