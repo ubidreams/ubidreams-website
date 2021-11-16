e687266a57cec20dae8f42556c4bb42be823e61e
EstelleUbi <estelle.tourillon@ubidreams.com>
2021-11-16 17:44:47 +0100

    fix slick issue
    (cherry picked from commit 3dbeb1a51315631cfa008c3556a39d3b5c579a45)
    
    # Conflicts:
    #	src/components/layout/footer/index.js
    #	src/components/layout/layout.js

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -8,108 +8,108 @@ import { CookiesContext } from '../../../helpers/cookiesContext'
 import { getAllLegalPages, getCoordonnees } from '../../../lib/api.js'
 
 const Footer = () => {
-  const cookies = useContext(CookiesContext)
-  const { t, lang } = useTranslation('common')
-  const [legalPages, setLegalPages] = useState([])
-  const [coordonnees, setCoordonnees] = useState({})
+	const cookies = useContext(CookiesContext)
+	const { t, lang } = useTranslation('common')
+	const [legalPages, setLegalPages] = useState([])
+	const [coordonnees, setCoordonnees] = useState({})
 
-  useEffect(() => {
-    getAllLegalPages(lang).then((data) => setLegalPages(data))
-    getCoordonnees(lang).then((data) => setCoordonnees(data))
-  }, [lang])
+	useEffect(() => {
+		getAllLegalPages(lang).then((data) => setLegalPages(data))
+		getCoordonnees(lang).then((data) => setCoordonnees(data))
+	}, [lang])
 
-  const MenuLink = ({ href, name }) => {
-    return (
-      <li className='mb-3'>
-        <Link href={'/' + href}>
-          <a className='text-reset'>{name}</a>
-        </Link>
-      </li>
-    )
-  }
+	const MenuLink = ({ href, name }) => {
+		return (
+			<li className='mb-3'>
+				<Link href={'/' + href}>
+					<a className='text-reset'>{name}</a>
+				</Link>
+			</li>
+		)
+	}
 
-  return (
-    <div style={{ marginTop: 'auto' }}>
-      <div className='position-relative'>
-        <div className='shape shape-bottom shape-fluid-x svg-shim text-gray-300'>
-          <svg viewBox='0 0 2880 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path d='M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z' fill='currentColor' />
-          </svg>
-        </div>
-      </div>
-      <footer className='py-8 py-md-11 bg-gray-300'>
-        <div className='container'>
-          <div className='row'>
-            {/* Logo + reseaux sociaux */}
-            <div className='col-12 col-md-4 col-lg-4'>
-              {/* <!-- Brand --> */}
-              <Image src={Ubidreams} alt='Logo Ubidreams' width={150} height={43} />
+	return (
+		<div style={{ marginTop: 'auto' }}>
+			<div className='position-relative'>
+				<div className='shape shape-bottom shape-fluid-x svg-shim text-gray-300'>
+					<svg viewBox='0 0 2880 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
+						<path d='M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z' fill='currentColor' />
+					</svg>
+				</div>
+			</div>
+			<footer className='py-8 py-md-11 bg-gray-300'>
+				<div className='container'>
+					<div className='row'>
+						{/* Logo + reseaux sociaux */}
+						<div className='col-12 col-md-4 col-lg-4'>
+							{/* <!-- Brand --> */}
+							<Image src={Ubidreams} alt='Logo Ubidreams' width={150} height={43} />
 
-              {/* <!-- Text --> */}
-              <p className='mb-2'>{t('footer.slogan')}</p>
+							{/* <!-- Text --> */}
+							<p className='mb-2'>{t('footer.slogan')}</p>
 
-              {/* <!-- Social --> */}
-              <ul className='list-unstyled list-inline list-social mb-6 mb-md-0'>
-                {t('footer.social', {}, { returnObjects: true }).map((socialMedia, index) => {
-                  return (
-                    <li key={index} className='list-inline-item list-social-item me-4 position-relative'>
-                      {cookies[socialMedia.cookieDesignation] ? (
-                        <a
-                          href={socialMedia.path}
-                          className='text-decoration-none'
-                          target='_blank'
-                          rel='noopener noreferrer'
-                        >
-                          <Image src={Img[socialMedia.img.key]} alt={socialMedia.img.alt} width={25} height={25} />
-                        </a>
-                      ) : (
-                        <>
-                          <div className='popover pop-footer bg-gray-200'>{t('common:cookie.noAccept')}</div>
-                          <div style={{ opacity: '0.6' }}>
-                            <Image src={Img[socialMedia.img.key]} alt={socialMedia.img.alt} width={25} height={25} />
-                          </div>
-                        </>
-                      )}
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-            {/* Location */}
-            <div className='col-12 col-md-4 col-lg-4'>
-              {/* <!-- Heading --> */}
-              <h6 className='fw-bold text-uppercase'>{t('footer.location.title')}</h6>
+							{/* <!-- Social --> */}
+							<ul className='list-unstyled list-inline list-social mb-6 mb-md-0'>
+								{t('footer.social', {}, { returnObjects: true }).map((socialMedia, index) => {
+									return (
+										<li key={index} className='list-inline-item list-social-item me-4 position-relative'>
+											{cookies[socialMedia.cookieDesignation] ? (
+												<a
+													href={socialMedia.path}
+													className='text-decoration-none'
+													target='_blank'
+													rel='noopener noreferrer'
+												>
+													<Image src={Img[socialMedia.img.key]} alt={socialMedia.img.alt} width={25} height={25} />
+												</a>
+											) : (
+												<>
+													<div className='popover pop-footer bg-gray-200'>{t('common:cookie.noAccept')}</div>
+													<div style={{ opacity: '0.6' }}>
+														<Image src={Img[socialMedia.img.key]} alt={socialMedia.img.alt} width={25} height={25} />
+													</div>
+												</>
+											)}
+										</li>
+									)
+								})}
+							</ul>
+						</div>
+						{/* Location */}
+						<div className='col-12 col-md-4 col-lg-4'>
+							{/* <!-- Heading --> */}
+							<h6 className='fw-bold text-uppercase'>{t('footer.location.title')}</h6>
 
-              {/* <!-- List --> */}
-              <div className='text-muted mb-6 mb-md-8 mb-lg-0' style={{ whiteSpace: 'pre-line' }}>
-                {coordonnees?.telephone}
-                <br />
-                {coordonnees?.email}
-                <br />
-                <div>{coordonnees?.adresse + ','}</div>
-                <div>{coordonnees?.ville + ','}</div>
-                <div>{coordonnees?.pays}</div>
-              </div>
-            </div>
-            {/* link mentions */}
-            <div className='col-12 col-md-4 col-lg-4'>
-              {/* <!-- Heading --> */}
-              <h6 className='fw-bold text-uppercase'>{t('footer.legal.title')}</h6>
+							{/* <!-- List --> */}
+							<div className='text-muted mb-6 mb-md-8 mb-lg-0' style={{ whiteSpace: 'pre-line' }}>
+								{coordonnees?.telephone}
+								<br />
+								{coordonnees?.email}
+								<br />
+								<div>{coordonnees?.adresse + ','}</div>
+								<div>{coordonnees?.ville + ','}</div>
+								<div>{coordonnees?.pays}</div>
+							</div>
+						</div>
+						{/* link mentions */}
+						<div className='col-12 col-md-4 col-lg-4'>
+							{/* <!-- Heading --> */}
+							<h6 className='fw-bold text-uppercase'>{t('footer.legal.title')}</h6>
 
-              {/* <!-- List --> */}
-              <ul className='list-unstyled text-muted mb-0'>
-                {legalPages &&
-                  legalPages.map((link, index) => {
-                    return <MenuLink key={index} href={link.slug} name={link.title} />
-                  })}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <Script src='../../js/axeptio.js' strategy='beforeInteractive' />
-      </footer>
-    </div>
-  )
+							{/* <!-- List --> */}
+							<ul className='list-unstyled text-muted mb-0'>
+								{legalPages &&
+									legalPages.map((link, index) => {
+										return <MenuLink key={index} href={link.slug} name={link.title} />
+									})}
+							</ul>
+						</div>
+					</div>
+				</div>
+				<Script src='../../js/axeptio.js' strategy='beforeInteractive' />
+			</footer>
+		</div>
+	)
 }
 
 export default Footer
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,32 +2,33 @@ import Head from 'next/head'
 import Footer from './footer'
 import Header from './header'
 
+
 export default function Layout({ children }) {
-  return (
-    <>
-      <Head>
-        <link rel='icon' href='/favicon.png' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <script async type='text/javascript' src='../../js/theme.bundle.js' />
-        <script async type='text/javascript' src='../../js/vendor.bundle.js' />
+	return (
+		<>
+			<Head>
+				<link rel='icon' href='/favicon.png' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<script async type='text/javascript' src='../../js/theme.bundle.js' />
+				<script async type='text/javascript' src='../../js/vendor.bundle.js' />
 
-        <meta property='og:locale' content='fr_FR' />
-        <meta property='og:locale:alternate' content='en_US' />
+				<meta property='og:locale' content='fr_FR' />
+				<meta property='og:locale:alternate' content='en_US' />
 
-        {/* La balise Meta pour les robots permet de contrôler avec précision la manière dont chaque page doit être indexée et présentée aux utilisateurs dans les résultats de recherche Google 
+				{/* La balise Meta pour les robots permet de contrôler avec précision la manière dont chaque page doit être indexée et présentée aux utilisateurs dans les résultats de recherche Google 
             - noindex : Indique de ne pas afficher cette page dans les résultats de recherche (pas d'indexation > dev/prod)
             - follow : Autorisation de suivre les liens internes de la page (découvrir d'autre page)
             - max-snippet: nombre maximal de caractères que vous souhaitez appliquer à un extrait de texte pour ce résultat de recherche (-1 : Google détermine la longueur d'extrait adaptée)
             - max-image-preview : Définissez la taille maximale d'un aperçu d'image pour cette page (large : un aperçu d'image de grande taille)
             - max-video-preview : nombre maximal de secondes que vous souhaitez appliquer à un extrait vidéo (-1 : la durée de l'extrait vidéo est illimitée.)
         */}
-        <meta name='robots' content='index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' />
-      </Head>
-      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <Header />
-        <div className='mt-11'>{children}</div>
-        <Footer />
-      </div>
-    </>
-  )
+				<meta name='robots' content='index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1' />
+			</Head>
+			<div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+				<Header />
+				<div className='mt-11'>{children}</div>
+				<Footer />
+			</div>
+		</>
+	)
 }
