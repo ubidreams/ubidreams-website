c0f598ae9fac4403eee3e102eff10d2828e5cdc2
EstelleUbi <estelle.tourillon@ubidreams.com>
2021-11-17 14:09:37 +0100

    fix linter

--- src/components/slide-item-partner.js
+++ src/components/slide-item-partner.js
@@ -3,39 +3,39 @@ import ReactHtmlParser from 'react-html-parser'
 import TypedReactHook from './typed'
 
 const SlidItemPartner = ({ config }) => {
-	const { img, nom, mandataire, description, url } = config
-	return (
-		<div className='card mb-6 mb-xl-0 px-4'>
-			<div className='card-img-top shadow-light'>
-				<Image
-					alt=''
-					data={{
-						...img.responsiveImage
-					}}
-					className='img-fluid object-contain img-regies'
-				/>
-			</div>
-			<div className='card-body'>
-				{url ? (
-					<a href={url} target='_blank' rel='noopener noreferrer'>
-						<h5 className='text-uppercase mb-1 text-dark-grey' style={{ fontWeight: 'unset' }}>
-							{nom}
-						</h5>
-					</a>
-				) : (
-					<h5 className='text-uppercase mb-2 text-dark-grey' style={{ fontWeight: 'unset' }}>
-						{nom + ' '} {mandataire && <TypedReactHook word={mandataire} />}
-					</h5>
-				)}
+  const { img, nom, mandataire, description, url } = config
+  return (
+    <div className='card mb-6 mb-xl-0 px-4'>
+      <div className='card-img-top shadow-light'>
+        <Image
+          alt=''
+          data={{
+            ...img.responsiveImage
+          }}
+          className='img-fluid object-contain img-regies'
+        />
+      </div>
+      <div className='card-body'>
+        {url ? (
+          <a href={url} target='_blank' rel='noopener noreferrer'>
+            <h5 className='text-uppercase mb-1 text-dark-grey' style={{ fontWeight: 'unset' }}>
+              {nom}
+            </h5>
+          </a>
+        ) : (
+          <h5 className='text-uppercase mb-2 text-dark-grey' style={{ fontWeight: 'unset' }}>
+            {nom + ' '} {mandataire && <TypedReactHook word={mandataire} />}
+          </h5>
+        )}
 
-				{description && (
-					<div className='mb-0' style={{ whiteSpace: 'pre-line', fontSize: '0.9em' }}>
-						{ReactHtmlParser(description)}
-					</div>
-				)}
-			</div>
-		</div>
-	)
+        {description && (
+          <div className='mb-0' style={{ whiteSpace: 'pre-line', fontSize: '0.9em' }}>
+            {ReactHtmlParser(description)}
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default SlidItemPartner
