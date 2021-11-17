06c8948b811ce4bf08c79267a643f8f57bbc91a6
EstelleUbi <estelle.tourillon@ubidreams.com>
2021-11-17 14:39:01 +0100

    fix: headers more strict

--- next.config.js
+++ next.config.js
@@ -5,26 +5,60 @@ const allRedirection = require('./redirection')
 
 module.exports = nextTranslate()
 module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-  here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true
+	/* bydefault config  option Read For More Optios
+	here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
+	cssModules: true
 })
-
+const securityHeaders = [
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on'
+	},
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block'
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN'
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff'
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin'
+	},
+	{
+		key: 'Content-Security-Policy',
+		value: 'default-src self'
+	}
+]
 module.exports = {
-  productionBrowserSourceMaps: true,
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'fr'
-  },
-  images: {
-    domains: ['www.datocms-assets.com']
-  },
-  reactStrictMode: false,
-  /* Add Your Scss File Folder Path Here */
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  },
-  async redirects() {
-    return allRedirection
-  }
+	productionBrowserSourceMaps: true,
+	i18n: {
+		locales: ['en', 'fr'],
+		defaultLocale: 'fr'
+	},
+	images: {
+		domains: ['www.datocms-assets.com']
+	},
+	reactStrictMode: false,
+	/* Add Your Scss File Folder Path Here */
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')]
+	},
+	async redirects() {
+		return allRedirection
+	},
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/(.*)',
+				headers: securityHeaders
+			}
+		]
+	}
 }
