680982a34c467eabea03b1c6ac059e69ffa55726
Adrien Vaury <adrien.vaury@ubidreams.com>
2023-01-05 11:26:47 +0100

    feat : google_analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
     });`
         }}
       />
-            <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=UA-24567240-2' />
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=UA-24567240-2' />
       <Script
         id='google-analytics'
         strategy='afterInteractive'
