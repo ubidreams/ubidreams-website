d1dd2ef3b17aef15e3dd8fb27ae7d32326aabdd8
Adrien Vaury <adrien.vaury@ubidreams.com>
2023-01-05 10:46:52 +0100

    feat : googleAnalytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import '../../public/css/theme.bundle.css'
 import '../../public/css/libs.bundle.css'
 import 'rc-pagination/assets/index.css'
 import '../styles/style.scss'
+import Script from 'next/script'
 
 // External Librairies
 import appWithI18n from 'next-translate/appWithI18n'
@@ -22,16 +23,31 @@ import Layout from '../components/layout/layout'
  */
 function MyApp({ Component, pageProps }) {
   return (
-    <LangProvider>
-      <CookiesProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </CookiesProvider>
-    </LangProvider>
+    <>
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX' />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+    });`
+        }}
+      />
+      <LangProvider>
+        <CookiesProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CookiesProvider>
+      </LangProvider>
+    </>
   )
 }
-
 export default appWithI18n(MyApp, {
   ...i18nConfig
 })
